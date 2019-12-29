f4bb2e47b442b8a6810c789067b87ea4e365155e
Effiong Effiong <f.young.de@gmail.com>
2019-12-29 01:32:10 +0100

    Added a 'Not Found' page to the app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import Navbar from './components/layouts/Navbar';
-import Users from './components/user/Users';
+import Home from './components/pages/Home';
 import User from './components/user/User';
-import Search from './components/user/Search';
 import Alert from './components/layouts/Alert';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
@@ -24,15 +24,10 @@ const App = () => {
               <div className="container">
                 <Alert />
                 <Switch>
-                  <Route exact path='/' render={(props) => (
-                    <React.Fragment>
-                      <Search />
-                      <Users/>
-    
-                    </React.Fragment>
-                  )} />
+                  <Route exact path='/' component={Home} />
                   <Route exact path='/user/:login' component={User} />
                   <Route exact path='/about' component={About} />
+                  <Route component={NotFound} />
                 </Switch>
               </div>
             </div>
